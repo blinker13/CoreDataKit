3f093fcef86b632842addb755abc644d8c9282bd
Felix Gabel <felix.gabel@me.com>
2014-06-13 18:54:11 +0200

    XCTextCase fixtures category files added
    Signed-off-by: Felix Gabel <felix.gabel@me.com>

---
+++ Tests/XCTestCase+Fixtures.h
@@ -0,0 +1,16 @@
+//
+//  XCTestCase+Fixtures.h
+//  CoreDataKit
+//
+//  Created by Felix Gabel on 13/06/14.
+//  Copyright (c) 2014 Felix Gabel. All rights reserved.
+//
+
+@import XCTest;
+
+
+@interface XCTestCase (Fixtures)
+
+@property (nonatomic, readonly) NSURL	*fixtureURL;
+
+@end

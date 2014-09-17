d3066605cc8d831f0c942da729ff5a4512e9d17f
Felix Gabel <felix.gabel@me.com>
2014-09-17 23:53:47 +0200

    Tests fixed

---
+++ Tests/StackTests.swift
@@ -0,0 +1,18 @@
+//
+//  StackTests.swift
+//  CoreDataKit
+//
+//  Created by Felix Gabel on 17/09/14.
+//  Copyright (c) 2014 Felix Gabel. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+
+class StackTests: XCTestCase {
+
+	func testExample() {
+		XCTAssert(true, "Pass")
+	}
+}
